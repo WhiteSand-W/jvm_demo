d52d0b3d8f970c64788c6e69f9be47f8703accba
W <w2837301874@163.com>
2022-09-29 12:14:50 +0800

    idea commit

---
+++ src/main/JvmTest.java
@@ -0,0 +1,11 @@
+package main;
+
+public class JvmTest {
+
+    public static void main(String[] args) {
+        long maxMemory = Runtime.getRuntime().maxMemory(); //返回Java虚拟机试图使用的最大内存量
+        long totalMemory = Runtime.getRuntime().totalMemory(); //返回Java虚拟机中的内存总量。
+        System.out.println("MAX_MEMORY=" + maxMemory + "(字节)、" + (maxMemory / (double)1024 / 1024) + "MB");//默认为内存的1/4
+        System.out.println("TOTAL_MEMORY=" + totalMemory + "(字节)、" + (totalMemory / (double)1024 / 1024) + "MB");//默认为内存的1/64
+    }
+}
